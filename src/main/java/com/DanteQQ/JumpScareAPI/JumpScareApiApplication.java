a8c015e87e02328f3cd3de5776186d9b544fceff
DanteQQ <radim.hejl@seznam.cz>
2023-08-31 10:18:33 +0200

    Initial commit

---
+++ src/main/java/com/DanteQQ/JumpScareAPI/JumpScareApiApplication.java
@@ -0,0 +1,13 @@
+package com.DanteQQ.JumpScareAPI;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JumpScareApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JumpScareApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/DanteQQ/JumpScareAPI/JumpScareApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.DanteQQ.JumpScareAPI;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JumpScareApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
