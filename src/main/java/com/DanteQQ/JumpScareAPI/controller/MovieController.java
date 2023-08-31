0dbaea88735d3539e46b7c5ce41063d752682cb3
DanteQQ <radim.hejl@seznam.cz>
2023-08-31 10:23:11 +0200

    Basic movie structure
    Basic movie model, controller and repository to create new Movie in database

---
+++ src/main/java/com/DanteQQ/JumpScareAPI/controller/MovieController.java
@@ -0,0 +1,21 @@
+package com.DanteQQ.JumpScareAPI.controller;
+
+import com.DanteQQ.JumpScareAPI.model.Movie;
+import com.DanteQQ.JumpScareAPI.repository.MovieRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class MovieController {
+    @Autowired
+    MovieRepository movieRepository;
+    @PostMapping
+    public ResponseEntity<Movie> createMovie(@RequestBody Movie movie) {
+        Movie savedMovie = movieRepository.save(movie);
+        return new ResponseEntity<>(savedMovie, HttpStatus.CREATED);
+    }
+}
